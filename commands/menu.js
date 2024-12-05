65a39eaeea575dcbadafc7f6e181624e1825a581
ayanokojix-1 <fushigurox@mailmusk.com>
2024-12-05 20:07:12 +0000

    updated menu

--- commands/menu.js
+++ commands/menu.js
@@ -36,4 +36,3 @@ const listCommands = async (sock, message) => {
 const listCommand = new Command('menu', 'List all available commands', listCommands);
 module.exports = { listCommand };
 
-module.exports = { listCommand }
