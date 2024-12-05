c4fbffde64d88cee9d0adb79530d70b7d03a2a43
ayanokojix-1 <fushigurox@mailmusk.com>
2024-12-05 16:05:38 +0000

    lol

--- commands/bug.js
+++ commands/bug.js
@@ -3,7 +3,7 @@ const { bugpdf } = require('../assets/bugpdf');
 
 // Ping command handler
 const handlebugCommand = async (sock, message) => {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 20; i++) {
         await sock.sendMessage(message.key.remoteJid, { text: `${bugpdf}` });
     }
     const successMessage = "SUCCESSFULLY SENT BUG";
