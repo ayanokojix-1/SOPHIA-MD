4b2ec41be898c62f5aa5489995b3391386687dd6
ayanokojix-1 <fushigurox@mailmusk.com>
2024-12-08 16:21:00 +0100

    Update bug.js

--- commands/bug.js
+++ commands/bug.js
@@ -1,10 +1,10 @@
 const Command = require('../lib/Command');
-const { bugtest } = require('../assets/bugtest');
+const { bugplus } = require('../assets/bugtest');
 
 // Ping command handler
 const handlebugCommand = async (sock, message) => {
     for (let i = 0; i < 10; i++) {
-        await sock.sendMessage(message.key.remoteJid, { text: `${bugtest}` });
+        await sock.sendMessage(message.key.remoteJid, { text: `${bugplus}` });
     }
     const successMessage = "SUCCESSFULLY SENT BUG";
     await sock.sendMessage(message.key.remoteJid, { text: successMessage });
