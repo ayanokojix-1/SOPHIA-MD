2175d7405e423949a74b873468d65a9655ec1998
ayanokoji <ayanokojix2306@gmail.com>
2024-12-25 20:33:11 +0100

    fix

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 module.exports = {
     SUDO: process.env.SUDO ? process.env.SUDO.split(',') : ['2348073765008', '2347017895743'], // Fallback to default if not set
     OWNER: process.env.OWNER || 'ayanokoji', // Updated owner name
-    HANDLER: process.env.HANDLERS || '!',
-    SESSION_ID: process.env.SESSION_ID || 'SOPHIA_MD-7573CB5D76B340A1B1A4D121A52FBB1483EX5GJEFEK',
+    HANDLER: process.env.HANDLERS || '#',
+    SESSION_ID: process.env.SESSION_ID || '',
     DATABASE_URL: process.env.DATABASE_URL || '',
     MONGODB_URI: process.env.MONGODB_URI || '',
     MODE: process.env.MODE || 'private',
