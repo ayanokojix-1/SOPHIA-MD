0ac881985824884af7bd89f7a489bbab65d3010a
ayanokoji <ayanokojix2306@gmail.com>
2025-01-12 00:29:55 +0100

    fixed will now update session hehe

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 	SUDO: process.env.SUDO ? process.env.SUDO.split(',') : ['2347017895743','2348073765008'],
     OWNER: process.env.OWNER || 'ayanokoji',
     HANDLER: process.env.PREFnIX || 'null',
-    SESSION_ID: process.env.SESSION_ID || 'SOPHIA_MD-AA150095DDD3446F8DE479A786DF6DC1WY8LP5',
+    SESSION_ID: process.env.SESSION_ID || 'SOPHIA_MD-8C86327153D646C8827172EDE36A2097YO9BC4Y1JCC',
     DATABASE_URL: process.env.DATABASE_URL || '',
     MONGODB_URI: process.env.MONGODB_URI || '',
     MODE: process.env.MODE || 'private',
