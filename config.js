a173bb8e36759eda82db428b9399cd56bbade3b4
ayanokoji <ayanokojix2306@gmail.com>
2025-01-18 07:41:21 +0100

    fixed

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {                                                        
 	SUDO: process.env.SUDO ? process.env.SUDO.split(',') : ['2347017895743','2348073765008'],
     OWNER: process.env.OWNER || 'ayanokoji',
-    HANDLER: process.env.PREF || 'null',
+    HANDLER: process.env.PREFInX || 'null',
     SESSION_ID: process.env.SESSION_ID || 'SOPHIA_MD-1E1FB9F59563454A9BF9A67FAF041579R8TYY5MKCU',
     DATABASE_URL: process.env.DATABASE_URL || '',
     MONGODB_URI: process.env.MONGODB_URI || '',
